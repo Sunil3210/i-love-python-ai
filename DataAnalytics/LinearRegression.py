fcc3d6241141672807da5841b94947b349e16481
Sunil3210 <ssj55322@gmail.com>
2025-01-05 18:30:34 +0530

    Linear Regression Changes

--- DataAnalytics/LinearRegression.py
+++ DataAnalytics/LinearRegression.py
@@ -4,6 +4,7 @@ Created on Sun Jan  5 14:35:55 2025
 
 @author: Sunil Kumar SJ
 """
+import numpy as np
 from math import sqrt
 def rmse_metrics(actual,predicted):
     predicted_error=0
@@ -12,4 +13,36 @@ def rmse_metrics(actual,predicted):
         predicted_error+=(y_act-y_predicted)**2
     mean_error=predicted_error/float(n)
     return sqrt(mean_error)
+
+def compute_coefficient(x,y):
+    #Here we have to get slope and intercept of the line
+    # slope is denoted by w1
+    #intercept or constant is denoted by w0
+    x_mean=np.mean(x)
+    y_mean=np.mean(y)
+    numerator=0;
+    denominator=0;
+    n=len(x);
+    for i in range(n):
+        numerator+=(x[i]-x_mean)*(y[i]-y_mean)
+        denominator+=(x[i]-x_mean)**2
+    slope=numerator/denominator
+    intercept=y_mean-(slope*x_mean)
+    return slope,intercept
+
+def predicted(x,w0,w1):
+    return x*w1+w0 #y=x1*w1+x2*w2+x3*w3+......+xn*wn+w0
+
+
+x = np.arange(1, 51)
+y = x*3+5
+
+# Add some random error to the array
+y[np.random.randint(0, len(y), size=10)] += np.random.randint(-5, 5)
+
+w1, w0 = compute_coefficient(x, y)
+y_hat = predicted(x,w1,w0)
+print("y_hat",y_hat)
+# display the value of predicted coefficients
+print(w1,w0)
 
